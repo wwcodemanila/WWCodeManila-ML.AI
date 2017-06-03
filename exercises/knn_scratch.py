50422ff7607605e56aef54e9a946d038603b544b
Isabelle Tingzon <ibtingzon2@gmail.com>
2017-06-03 17:00:57 +0800

    Fixed broken link

--- exercises/knn_scratch.py
+++ exercises/knn_scratch.py
@@ -1,7 +1,3 @@
-import pandas as pd
-import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
 # 0. Import pandas, numpy
 #    from sklearn, import train_test_split, accuracy_score
 
