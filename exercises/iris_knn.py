6e4e9e8ee6e312918af5adf231f8f73989781741
Isabelle Tingzon <ibtingzon2@gmail.com>
2017-06-03 14:04:04 +0800

    Edited exercise 2

--- exercises/iris_knn.py
+++ exercises/iris_knn.py
@@ -1,7 +1,7 @@
 # 0. Import the following libraries:
-# 	 pandas, numpy
-# 	 matplotlib.pyplot as plt
-# 	 sklearn libraries for KNN, accuracy_score, train_test_split, and cross_val_score
+#    pandas, numpy
+#    matplotlib.pyplot as plt
+#    sklearn libraries for KNN, accuracy_score, train_test_split, and cross_val_score
 
 ###################################
 # PART 1: MACHINE LEARNING BASICS #
@@ -12,7 +12,7 @@ filename = None
 df = None
 
 # 2. Create matrix X containing the features
-# 	 Create target vector y containing the classes
+#    Create target vector y containing the classes
 X = None
 y = None
 
@@ -22,7 +22,7 @@ y = None
 X_train, X_test, y_train, y_test = None, None, None, None
 
 # 4. Instantiate Learning Model for KNN. 
-# 	 Set K = 3 (i.e. n_neighbors)
+#    Set K = 3 (i.e. n_neighbors)
 
 # 5. Fit KNN model to training set
 
@@ -44,22 +44,22 @@ print(neighbors)
 
 cv_scores = []
 # 3. For each k in neighbors,
-# 	 	3.1 Instantiate a learning model for KNN and set parameter n_neighbors = k
-# 	 	3.2 Perform cross validation using the cross_val_score function (set number of folds cv=10, scoring='accuracy')
-#	    3.3 Set avg_score to the average of the scores returned by cross_val_score
-# 	 	3.3 Append avg_score to the list cv_scores 
+#       3.1 Instantiate a learning model for KNN and set parameter n_neighbors = k
+#       3.2 Perform cross validation using the cross_val_score function (set number of folds cv=10, scoring='accuracy')
+#       3.3 Set avg_score to the average of the scores returned by cross_val_score
+#       3.4 Append avg_score to the list cv_scores 
 
 mse = []
 # 4. Get the misclassification errors and store them in list 'mse'
 #    For each score in cv_scores:
-#	    4.1 Set the misclassification error  to (1- cv_score)
-# 		4.2 Append the misclassification error to the list mse
+#       4.1 Set the misclassification error  to (1- cv_score)
+#       4.2 Append the misclassification error to the list mse
 
 optimal_k = None
 # 5. Get the optimal k
-# 	 5.1. Set min_val = minimum value in mse (code for getting minimum value is: min(mse))
-# 	 5.2. Set index_min = index of the minimum value in mse (mse.index(min_val))
-# 	 5.3. Set optimal_k as the value of neighbor at index_min
+#    5.1. Set min_val = minimum value in mse (code for getting minimum value is: min(mse))
+#    5.2. Set index_min = index of the minimum value in mse (mse.index(min_val))
+#    5.3. Set optimal_k as the value of neighbors at index_min
 
 print("Optimal value for k is: ", optimal_k)
 
