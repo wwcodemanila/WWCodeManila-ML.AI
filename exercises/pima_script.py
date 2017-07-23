77a5f501e8569282717d3961bca4585c899d7ce4
Isabelle Tingzon <ibtingzon2@gmail.com>
2017-07-23 15:47:04 +0800

    converted most tutorials to .ipynb

--- exercises/pima_script.py
+++
@@ -1,9 +0,0 @@
-"""
-Dataset: datasets/pima.csv
-
-Follow the step-by-step tutorial here: http://machinelearningmastery.com/handle-missing-data-python/
-
-The tutorial uses LinearDiscriminantAnalysis as its classifier, but you can choose any that you like. 
-"""
-
-filename = '../datasets/pima.csv'
